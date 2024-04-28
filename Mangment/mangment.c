75c6d21327d23727dd9bf097808cf58d26ae4fd3
amr <amrkhaledelsisy@gmail.com>
2024-04-29 01:36:54 +0300

    Data Mangement &File Handling

--- Mangment/mangment.c
+++ Mangment/mangment.c
@@ -1,4 +1,89 @@
 #include "include.h"
+u32 Logout;
+void system_init()
+{
+	u8 i=0;
+	while(1)
+	{
+		if(i++!=0)
+		{
+			
+		
+		   while(1)
+		   {
+			
+			    printf("\n\n                                      If You Want Continue Enter Any Number Except 0\n ");
+			    ES current_error_state=take_number(&Logout);
+		        Error(current_error_state);
+			    if(current_error_state==valid) break;
+		    }
+		  	if(Logout==0)
+		    {
+		      	Logout=1;
+				clearScreen();
+			    break;
+		    } 
+		    else
+		    {
+			    Logout=0;
+		    }
+		  
+		}
+	
+
+		u32 choice=0,i=0;
+		if(choice==1||choice==2||i==0){
+			//decoration
+
+		printf("\n\n"
+		"\n\t\t\t        *********************************************"
+		"\n\t\t\t        =                  WELCOME                  ="
+		"\n\t\t\t        =                    TO                     ="
+		"\n\t\t\t        =                 Om Ahmed                  ="
+		"\n\t\t\t        =               Student Record              ="
+		"\n\t\t\t        =                 MANAGEMENT                ="
+		"\n\t\t\t        =                   SYSTEM                  ="
+		"\n\t\t\t        *********************************************"
+		"\n\n");
+
+		i++;
+		}
+		while(1)
+		{
+					//admin or user
+			printf("                     1-->Admin                                                  2-->User\n\n");
+			ES current_error_state=take_number(&choice);
+			Error(current_error_state);
+			if(current_error_state==valid) break;
+		}
+		clearScreen();
+		switch(choice)
+		{
+			case 1:
+			      if(login(choice)==valid)
+				  {
+					  Admin_choice();
+				  }
+			break;
+
+			case 2:
+					if(login(choice)==valid)
+					{
+						user_choice();
+					}
+			break;
+
+		}
+		if(Logout) {
+		clearScreen();
+		printf("                            Bya Bya Htw7shniiiiiiiiii ");
+
+		break;
+    }
+	}
+
+}
+
 extern struct STUDENTS * Head;
 struct STUDENTS* HELP=NULL;
 char* takestring_v2(){
--- Mangment/mangment.h
+++ Mangment/mangment.h
@@ -1,7 +1,9 @@
 #ifndef mang
 #define mang
 #include "Std_Type.h"
+//Function receives any input k string
 char * takestring_v2();
+//To handile error
 typedef enum
 {
 	overflow,
@@ -10,10 +12,18 @@ typedef enum
 	notnumber
 
 }ES;
+//It takes the error type to and prints a message based on it
 void Error(ES current_error_state);
+//He takes the input and sees if it is correct or error
 ES take_number(u32 *choise);
+//Save data hhhhhhh 
 void save_data();
+//load data hhhhhhhh
 void load_data();
+//btm7iiiiiiiiiiiiiiiiiiiiiiiii
 void clear_list();
+//btms7 terminal to decoration
 void clearScreen();
+//system kolo
+void system_init();
 #endif
